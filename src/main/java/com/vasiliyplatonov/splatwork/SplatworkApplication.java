f0a33da6098e9f1b4242f02875fb2d5106ee00b1
VasiliyPlatonov <vasiliyplat@gmail.com>
2018-09-26 22:54:20 +0300

    first commit

---
+++ src/main/java/com/vasiliyplatonov/splatwork/SplatworkApplication.java
@@ -0,0 +1,12 @@
+package com.vasiliyplatonov.splatwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplatworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplatworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vasiliyplatonov/splatwork/SplatworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vasiliyplatonov.splatwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SplatworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
