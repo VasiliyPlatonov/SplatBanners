414df30c318c743e949d65c4b0d9363c3f1dcf20
VasiliyPlatonov <vasiliyplat@gmail.com>
2018-09-28 14:21:48 +0300

    Preparation for the start of the project is completed.

--- src/main/java/com/vasiliyplatonov/splatwork/SplatworkApplication.java
+++ src/main/java/com/vasiliyplatonov/splatwork/SplatworkApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplatworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SplatworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SplatworkApplication.class, args);
+    }
 }
---
+++ src/main/java/com/vasiliyplatonov/splatwork/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.vasiliyplatonov.splatwork.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value="/login")
+    public String login(){
+        return "login";
+    }
+
+}
