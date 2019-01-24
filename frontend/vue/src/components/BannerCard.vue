3bb84c1afcc5dd251ad99dd4fdc1dd8a2081e35e
VasiliyPlatonov <vasiliyplat@gmail.com>
2019-01-24 14:11:30 +0300

    Refactoring Banner view. Added new component BannerCard

---
+++ frontend/vue/src/components/BannerCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-hover>
+      <v-card
+        slot-scope="{ hover }"
+        :class="`elevation-${hover ? 10 : 3}`"
+        class="banner"
+      >
+
+        <a :href="banner.targetUrl">
+          <v-img
+            :src="banner.imgSrc"
+            aspect-ratio="0.9"
+            cover
+          ></v-img>
+        </a>
+
+        <v-card-title>
+          <div class="banner__title-wrap">
+            <h3 class="headline">Id: {{ banner.id }}</h3>
+            <p>Width: {{ banner.width }}</p>
+            <p>Height: {{ banner.height }}</p>
+            <p>Target url:
+              <a class="banner__targetUrl" :href="banner.targetUrl">
+                {{ banner.targetUrl }}
+              </a>
+            </p>
+            <p>Banner langId: {{ banner.langId }}</p>
+          </div>
+        </v-card-title>
+
+      </v-card>
+    </v-hover>
+</template>
+
+<script>
+  export default {
+    props: ['banner']
+  }
+</script>
+
+<style lang="less">
+  .banner {
+    margin-bottom: 15px;
+    margin-right: 5px;
+    margin-left: 5px;
+
+    p {
+      margin-bottom: 5px;
+    }
+
+    &__targetUrl {
+      max-width: 100%;
+      display: inline-block;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    &__title-wrap {
+      max-width: 100%;
+    }
+  }
+</style>
--- frontend/vue/src/views/Banners.vue
+++ frontend/vue/src/views/Banners.vue
@@ -10,41 +10,19 @@
 
     <v-container grid-list-md>
       <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3
+        <v-flex xs12 sm3 lg2
                 v-for="banner in banners"
                 :key="banner.id"
         >
-          <v-card class="mb-3">
-            <a :href="banner.targetUrl">
-              <v-img
-                :src="banner.imgSrc"
-                aspect-ratio="1.7"
-                contain
-
-              ></v-img>
-            </a>
-            <v-card-title primary-title>
-              <h3 class="headline mb-0">Id: {{ banner.id }}</h3>
-              <p>Width: {{ banner.width }}</p>
-              <p>Height: {{ banner.height }}</p>
-              <span>Target url:</span>
-              <a :href="banner.targetUrl">
-                {{ banner.targetUrl }}
-              </a>
-              <p>Banner langId: {{ banner.langId }}</p>
-            </v-card-title>
-
-            <v-card-actions>
-              <v-btn flat color="orange">Share</v-btn>
-              <v-btn flat color="orange">Explore</v-btn>
-            </v-card-actions>
-          </v-card>
+          <banner :banner="banner"></banner>
         </v-flex>
       </v-layout>
     </v-container>
   </div>
 </template>
 <script>
+  import banner from '../components/BannerCard.vue'
+
   export default {
     computed: {
       banners: {
@@ -56,6 +34,11 @@
     mounted() {
       this.banners = this.$store.dispatch('setBanners')
     },
+    components: {
+      banner
+    }
   }
 </script>
+
+
 
